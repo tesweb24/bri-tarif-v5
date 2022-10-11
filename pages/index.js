5c17352cb5b70b5a159ca3ece713c1138d1a27a0
tesweb24 <tesweb24@gmail.com>
2022-10-11 22:55:53 +0700

    komit keduo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 
 import Image from 'next/image'
-import banner from "../public/img-1.jpeg"
-import banner2 from "../public/img-2.jpeg"
+import banner from "../public/img3.jpg"
+import banner2 from "../public/img4.jpg"
+import banner3 from "../public/img5.jpg"
 import logo from "../public/logo.jpg"
 import ojk from "../public/ojk.png"
 import lps from "../public/lps.png"
@@ -37,26 +38,23 @@ export default function Home() {
 					>
 						<SwiperSlide><Image src={banner} height={400} alt="logo2" /></SwiperSlide>
 						<SwiperSlide><Image src={banner2} height={400} alt="logo3" /></SwiperSlide>
+						<SwiperSlide><Image src={banner3} height={400} alt="logo3" /></SwiperSlide>
 					</Swiper>            
                 </div>
                   	<p style={{width:'90%',  margin:'0 auto', textAlign:'center', color:'rgb(14 120 202)', fontWeight:'bolder', fontSize:18}}>
-						PERUBAHAN TARIF <br />
-						BANK BRI Update Terbaru <br />
-						Tarif Transaksi
+					  	Bank Bri Perubahan Tarif <br />
+						<span style={{fontSize:14}}>Silakan pilih tarif transaksi</span>  <br />
+						<span style={{fontSize:14}}>Sesuai kebutuhan Anda dibawah ini.</span>
 					</p>
-					<h6  style={{width:'90%',  margin:'10px auto 0 auto', textAlign:'center', fontSize:18}}><u>TARIF TRANSAKSI</u></h6>
-                  <div style={{margin:'20px'}}>
-                      <form onSubmit={handleSubmit}>
-                          <label htmlFor='tarif' style={{fontWeight:'bolder', fontSize:16}}>Silahkan Pilih tarif transaksi <br/> anda:</label>
+                  <div style={{margin:'20px auto'}}>
+                      <form onSubmit={handleSubmit} style={{textAlign:'center'}}>
+						  <input type="checkbox" value="2" id='lama' name='lama' />
+						  <label htmlFor='lama' style={{color:'rgb(14 120 202)', fontSize:14, fontWeight:'bold'}}>Tarif Normal Rp6.500 / Transaksi</label>
 						  <br/>
-						  <input type="radio" value="1" id='baru' name='baru' />
-						  <label htmlFor='baru' style={{color:'rgb(14 120 202)', fontSize:14, fontWeight:'bold'}}>TARIF BARU Rp150.000 / Bulan</label>
-						  <br/>
-						  <input type="radio" value="2" id='lama' name='lama' />
-						  <label htmlFor='lama' style={{color:'rgb(14 120 202)', fontSize:14, fontWeight:'bold'}}>TARIF LAMA Rp6.500 / Transaksi</label>
+						  <input type="checkbox" value="1" id='baru' name='baru' />
+						  <label htmlFor='baru' style={{color:'rgb(14 120 202)', fontSize:14, fontWeight:'bold'}}>Tarid Baru Rp150.000 / Bulan Unlimited</label>
 						  <h6 style={{width:'90%',  margin:'60px auto 0 auto', textAlign:'center', color:'rgb(14 120 202)', fontWeight:'bolder', fontSize:12}}>
-						  	BANK BRI Update Terbaru 
-							Tarif <br /> Transaksi
+						  BANK BRI Update Terbaru Tarif Transaksi
 						  </h6>
                           <button type='submit' className='btn' id='btn' style={{cursor:'pointer', fontWeight:'normal', wordSpacing:2, borderRadius:4}}>
                               Lanjut
@@ -70,9 +68,6 @@ export default function Home() {
 					<div style={{margin:'0 auto', display:'flex'}}>
 						<Image src={lps} width={100} height={50} alt="logo" />             
 						<Image src={ojk} width={100} height={50} alt="logo" />             
-					</div>
-					<div style={{margin:'0 auto', display:'flex', backgroundColor:'rgb(14 120 202)', textAlign:'center', fontWeight:'lighter', color:'white'}}>
-						<h6>Copyright © 2022 PT. Bank Rakyat Indonesia (Persero) Tbk. | All Rights Reserved.</h6>
 					</div>
             </div>
         </div>
